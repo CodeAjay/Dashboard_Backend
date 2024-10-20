a426b1ca657e44213335d52d5c954affc6408a7b
codeajay <ajay01937@gmail.com>
2024-10-20 22:29:29 +0530

    jj

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ const students=require("./routes/studentsRoutes/student")
 app.use("/login", loginRoute); // Public route to log in
 
 // Admin-only routes
-app.use("/announcements", authenticate, authorizeRoles("admin"), announceRoutes);
-app.use("/courses", authenticate, authorizeRoles("admin"), courseRoutes);
-app.use("/students", authenticate, authorizeRoles("admin", "clerk"), studentRoutes); // Admin and clerk
-app.use("/institutes", authenticate, authorizeRoles("admin"), instituteRoutes);
-app.use("/fee-collection", authenticate, authorizeRoles("admin", "clerk"), feeCollectionRoutes); // Admin and clerk
+app.use("/announcements", announceRoutes);
+app.use("/courses", courseRoutes);
+app.use("/students", studentRoutes); // Admin and clerk
+app.use("/institutes", instituteRoutes);
+app.use("/fee-collection", feeCollectionRoutes); // Admin and clerk
 
 
 // Students Routes
-app.use("/api", authenticate, authorizeRoles("admin", "clerk"), students)
+app.use("/api", students)
 
 
 app.listen(port, () => {
